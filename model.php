bdb27396bb23aefd0346390e48dfb45c3463e579
leNigaud <rafeno231@gmail.com>
2023-07-12 17:23:05 +0300

    database OK

--- model.php
+++ model.php
@@ -5,7 +5,7 @@
     $password= "";
 
     try {
-        $conn = new PDO("mysql:host=$host; dbname = $dbName", $username, $password);
+        $conn = new PDO("mysql:host=$host; dbname=$dbName", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e) {
         die("DB connect error: " . $e->getMessage());
