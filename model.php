d2129da90e6c3c89d1bafdf6c93661ef91f9d425
leNigaud <111077740+leNigaud@users.noreply.github.com>
2023-07-12 16:02:44 +0300

    initial commit

---
+++ model.php
@@ -0,0 +1,55 @@
+<?php
+    $host = "localhost";
+    $dbName = "vue";
+    $username = "root";
+    $password= "";
+
+    try {
+        $conn = new PDO("mysql:host=$host; dbname = $dbName", $username, $password);
+    }catch(PDOException $e) {
+        die("DB connect error: " . $e->getMessage());
+    }
+
+    $dataReceived = json_decode(file_get_contents("php://input"));
+
+    if($dataReceived->action == 'fetchall') {
+        $query = "SELECT * FROM etudiant";
+        $statement = $conn->query($query);
+        $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        print(json_encode($data));
+    }
+
+    else if($dataReceived->action == 'insert') {
+        $numEt = $dataReceived->numEt;
+        $nom = $dataReceived->nom;
+        $note_math = $dataReceived->note_math;
+        $note_phys = $dataReceived->note_phys;
+        
+        $query = "INSERT INTO etudiant (numEt, nom, note_math, note_phys) VALUES (?, ?, ?, ?)";
+
+        $statement = $conn->prepare($query);
+        $statement->execute([$numEt, $nom, $note_math, $note_phys]);
+    }
+
+    else if($dataReceived->action == 'update') {
+        $ancientNum = $dataReceived->$ancientNum;
+        $numEt = $dataReceived->numEt;
+        $nom = $dataReceived->nom;
+        $note_math = $dataReceived->note_math;
+        $note_phys = $dataReceived->note_phys;        
+
+        $statement = $conn->prepare($query);
+        $query = "UPDATE etudiant SET numEt=?, nom=?, note_math=?, note_phys=? WHERE numEt=?";
+
+        $statement->execute([$numEt, $nom, $note_math, $note_phys, $ancientNum]);
+
+    }
+
+    else if($dataReceived->action == 'delete') {
+        $numEt = $dataReceived->numEt;
+        $query = "DELETE FROM etudiant WHERE numEt = ?";
+        
+        $statement = $conn->prepare($query);
+        $statement->execute([$numEt]);
+    }
